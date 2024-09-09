7c590e55db2d3de86a4e03aa6fa15d43c7edbb21
dinizdv <dinizdev3@gmail.com>
2024-09-09 15:33:39 -0300

    feat(Communities): communities interface updated.

--- whatsapp/src/Components/Communities/index.jsx
+++ whatsapp/src/Components/Communities/index.jsx
@@ -16,27 +16,31 @@ const Communities = () => {
                                 
                                     <div className="community">
                                 <button id='btn-new-community'><i class="fa-solid fa-users"></i></button>
-                                <label htmlFor="" id='community-title'>Coding in Community</label>    
+                                <label htmlFor="" id='community-title'>Code is Life</label>    
                                     </div>
                     
 
                                     <div className="community">
-                                <button id='btn-new-community'><i class="fa-solid fa-bullhorn"></i></button>
+                                <button id='btn-announcement'><i class="fa-solid fa-bullhorn"></i></button>
                                 <label htmlFor="">Announcements</label>    
                                     </div>
                     
                                     <div className="community">
-                                <button id='btn-new-community'><i class="fa-brands fa-react"></i></button>
+                                <button id='btn-reactjs'><i class="fa-brands fa-react"></i></button>
                                 <label htmlFor="">Coding in React js</label>    
                                     </div>
+
                                     <div className="community">
-                                <button id='btn-new-community'><i class="fa-brands fa-vuejs"></i></button>
-                                <label htmlFor="">Coding in Vue js</label>    
+                                <button id='btn-java'><i class="fa-brands fa-js"></i></button>
+                                <label htmlFor="">Coding in JavaScript</label>    
                                     </div>
 
                             </div>
 
-                    
+                            <div className="container-btn-view-channel">
+        <button>View all</button>
+        </div>
+
                             </div>
     )
 }
